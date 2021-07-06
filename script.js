c5c48c3affeeca839fff1eebbf9bccb05b578d7c
PatrikG <Patrik0Gn@gmail.com>
2021-07-06 08:40:08 +0200

    Add name input and display basic bmi list

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const calculated = document.querySelector("h2");
 const bmiForm = document.querySelector(".bmiForm");
+const bmiList = document.querySelector(".bmi-list");
+
+//const bmiArray = [{ name: "TomTest", bmi: "25", text: "nice" }];
 
 bmiForm.btn__calc.addEventListener("click", () => {
+  const name = bmiForm.name.value;
   const height = bmiForm.height.value;
   const weight = bmiForm.weight.value;
   const bmi = (weight / height ** 2) * 10000;
+
+  const bmiObject = {
+    name,
+    bmi,
+    text: "Miau",
+  };
+
+  const bmiListNew = document.createElement("li");
+  bmiListNew.textContent = `${bmiObject.name} ${bmiObject.bmi.toFixed(2)} ${
+    bmiObject.text
+  }`;
+  //console.log(bmiElementName);
+  bmiList.append(bmiListNew);
 
   calculated.textContent = `Your BMI is ${bmi.toFixed(2)}`;
 
@@ -27,3 +44,17 @@ bmiForm.btn__calc.addEventListener("click", () => {
 // between 18.5 and 25 (not inclusive) = green
 // between 25 and 30 (not inclusive) = orange
 // from 30 on = red
+
+/*
+for (let i = 0; i < bmiObject.length; i++) {
+  const bmiObjElement = bmiObject[i];
+  const bmiElementName = bmiObjElement.name;
+  const bmiElementValue = bmiObjElement.bmi;
+  const bmiElementtext = bmiObjElement.text;
+
+  const bmiListNew = document.createElement("li");
+  bmiListNew.textContent = `${bmiElementName} ${bmiElementValue} ${bmiElementtext}`;
+  console.log(bmiElementName);
+  bmiList.append(bmiListNew);
+}
+*/
